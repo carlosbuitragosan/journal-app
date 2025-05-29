de113900707389594b113876440bbad976d8e90d
Carlos Buitrago <letscatchupagain@gmail.com>
2025-05-29 10:35:14 +0100

    refactor: remove card container from guest layout for cleaner welcome page.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -28,15 +28,7 @@
     <div
       class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900"
     >
-      <div class="text-xl font-bold text-white">
-        <h1 class="text-4xl font-extrabold text-white">Reflekt</h1>
-      </div>
-
-      <div
-        class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"
-      >
-        {{ $slot }}
-      </div>
+      {{ $slot }}
     </div>
   </body>
 </html>
